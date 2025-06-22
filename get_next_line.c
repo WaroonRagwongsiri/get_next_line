322aeb7588fee04a2202040c3046fcaaf64111f4
WaroonRagwongsiri <waroonwork@gmail.com>
2025-06-22 22:40:57 +0700

    now one byte at a time almost work

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/21 12:55:00 by marvin            #+#    #+#             */
-/*   Updated: 2025/06/22 22:29:21 by marvin           ###   ########.fr       */
+/*   Updated: 2025/06/22 22:39:09 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ char	*get_next_line(int fd)
 	char	buffer[2];
 	char	*sol;
 	ssize_t	bytes_read;
+
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
 	sol = ft_calloc(1, 1);
@@ -31,7 +32,7 @@ char	*get_next_line(int fd)
 	{
 		bytes_read = read(fd, buffer, 1);
 		buffer[bytes_read] = '\0';
-		sol = append_n_str(sol, buffer, 0);
+		sol = append_n_str(sol, buffer, 1);
 		if (!sol)
 			return (NULL);
 		if (buffer[0] == '\n')
